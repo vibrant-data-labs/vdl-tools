f62681cfc51dc9f4d337d056159770db8c134c7a
Zein Tawil <zein@vibrantdatalabs.org>
2025-10-29 13:10:56 -0700

    making openai api calls simpler and embeddings

--- vdl_tools/shared_tools/openai/embedding_cache.py
+++ vdl_tools/shared_tools/openai/embedding_cache.py
@@ -144,7 +144,7 @@ class EmbeddingCache():
             given_id=given_id,
             input_text=text,
             response_full={"data": response},
-            embedding=np.array(response),
+            embedding=response,
         )
 
         self.session.merge(embedding_obj)
