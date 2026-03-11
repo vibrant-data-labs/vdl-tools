d0c17e3cb3b0932a6180f480f2fbf3e0813029b7
Zein Tawil <zein@vibrantdatalabs.org>
2026-03-11 11:57:29 -0700

    NZI Search API Updates

--- vdl_tools/scrape_enrich/netzero_insights/search_netzero_api.py
+++ vdl_tools/scrape_enrich/netzero_insights/search_netzero_api.py
@@ -34,6 +34,8 @@ def create_search_filter(
     exclude_headquarters: list[str] = None,
     include_startup_ids: list[int] = None,
     exclude_startup_ids: list[int] = None,
+    minimum_funding: int = None,
+    maximum_funding: int = None,
     include_investors: list[int] = None,
     exclude_investors: list[int] = None,
     include_taxonomy_items: list[int] = None,
@@ -75,6 +77,14 @@ def create_search_filter(
     if name:
         startup_include_filter = startup_include_filter or StartupFilter()
         startup_include_filter.name = name
+
+    if minimum_funding:
+        startup_include_filter = startup_include_filter or StartupFilter()
+        startup_include_filter.fundingsFrom = minimum_funding
+
+    if maximum_funding:
+        startup_include_filter = startup_include_filter or StartupFilter()
+        startup_include_filter.fundingsTo = maximum_funding
 
     if include_headquarters:
         startup_include_filter = startup_include_filter or StartupFilter()
@@ -199,6 +209,7 @@ def get_full_details_from_company_ids(
     company_ids: list[int],
     return_investor_details: bool = True,
     return_funding_rounds: bool = True,
+    get_commercial_deals: bool = False,
     use_sandbox: bool = False,
     read_from_cache: bool = True,
     write_to_cache: bool = True,
@@ -248,6 +259,16 @@ def get_full_details_from_company_ids(
             netzero_api=netzero_api,
         )
         return_data["investor_details"] = pd.DataFrame(investor_details)
+
+    if get_commercial_deals:
+        commercial_deals = get_company_commercial_deals(
+            company_ids=company_ids,
+            use_sandbox=use_sandbox,
+            read_from_cache=read_from_cache,
+            write_to_cache=write_to_cache,
+            netzero_api=netzero_api,
+        )
+        return_data["commercial_deals"] = pd.DataFrame(commercial_deals)
 
     return return_data
 
